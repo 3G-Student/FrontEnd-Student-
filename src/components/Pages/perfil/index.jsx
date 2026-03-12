9949ca86b153fcc8019e126c4dc660811a22c686
NathanFGerminare <nathan.barreto@germinare.org.br>
2026-03-12 20:57:20 -0300

    Ajuste de perfil

--- src/components/Pages/perfil/index.jsx
+++ src/components/Pages/perfil/index.jsx
@@ -1,29 +1,74 @@
 import "./perfil.css";
 import { useNavigate } from "react-router-dom";
 import { FaUpload, FaArrowLeft } from "react-icons/fa";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 export default function PerfilProfessor() {
+
   const navigate = useNavigate();
+
   const [popupSucesso, setPopupSucesso] = useState(false);
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
+  const [novaSenha, setNovaSenha] = useState("");
+
   const backendURL = import.meta.env.VITE_BACKEND_URL;
 
-  const atualizarSenha = async () => {
-    const idUsuario = localStorage.getItem("idUsuario");
+  // regex validação senha
+  const validarSenha = (senha) => {
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/;
+    return regex.test(senha);
+  };
+
+  // buscar perfil
+  useEffect(() => {
+    const usuarioId = localStorage.getItem("idUsuario");
     const token = localStorage.getItem("token");
   
-    console.log("ID USUARIO:", idUsuario);
-    console.log("TOKEN:", token);
-  
+    if (!usuarioId) {
+      console.error("ID do usuário não encontrado");
+      return;
+    }
+    fetch(`${backendURL}/api/Aluno/perfil/${usuarioId}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao buscar perfil");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setNome(data.nome);
+      setEmail(data.email);
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+    });
+  }, []);
+
+  // atualizar senha
+  const atualizarSenha = async () => {
+
+    const idUsuario = localStorage.getItem("idUsuario");
+    const token = localStorage.getItem("token");
+
     if (!idUsuario) {
       alert("Usuário não encontrado");
       return;
     }
-  
+
+    if (!validarSenha(novaSenha)) {
+      alert(
+        "A senha deve ter no mínimo 8 caracteres, incluindo: 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractere especial."
+      );
+      return;
+    }
+
     try {
       const response = await fetch(
         `${backendURL}/api/Usuario/atualizarSenha/${idUsuario}`,
@@ -34,64 +79,62 @@ export default function PerfilProfessor() {
             Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({
-            novaSenha: senha,
+            novaSenha: novaSenha,
           }),
         }
       );
-  
       if (!response.ok) {
         throw new Error("Erro ao atualizar senha");
       }
-  
       setPopupSucesso(true);
       setTimeout(() => setPopupSucesso(false), 3000);
-
-      setSenha("");
-  
+      setNovaSenha("");
     } catch (error) {
       console.error(error);
       alert("Erro ao atualizar senha");
     }
   };
+
   return (
     <div className="perfil-page">
+
       <aside className="perfil-sidebar">
-        <div
-          className="sidebar-perfil"
-          onClick={() => navigate("/professores")}
-        >
-          <div className="avatar-sidebar">
-            <FaArrowLeft />
-          </div>
+        <div className="sidebar-perfil" onClick={() => navigate("/professores")}>
+          <div className="avatar-sidebar"><FaArrowLeft /></div>
         </div>
       </aside>
+
       <div className="perfil-content">
+        <div className="mobile-back-button" onClick={() => navigate("/professores")}>
+          <FaArrowLeft />
+        </div>
 
         <header className="perfil-header">
-          <div className="header-left">
 
+          <div className="header-mobile">
+            <FaArrowLeft className="mobile-back" onClick={() => navigate("/professores")}/>
+          </div>
+
+          <div className="header-left">
             <div>
               <h2>Perfil</h2>
               <span className="sub">Professor</span>
             </div>
           </div>
 
-          <span
-            className="student-tag"
-            onClick={() => navigate("/professores")}
-          >
+          <span className="student-tag" onClick={() => navigate("/professores")}>
             STUDENT <span>+</span>
           </span>
+
         </header>
+        <div className="mobile-back-button" onClick={() => navigate("/professores")}>
+          <FaArrowLeft />
+        </div>
 
         <div className="perfil-card">
           <div className="foto-section">
             <div className="foto-box">
-              <img
-                src="https://picsum.photos/200"
-                alt="Foto de perfil"
-                className="perfil-img"
-              />
+              <img src="https://picsum.photos/200" alt="Foto de perfil" className="perfil-img"/>
             </div>
 
             <button className="upload-btn">
@@ -99,38 +142,21 @@ export default function PerfilProfessor() {
               Fazer upload de nova foto
             </button>
           </div>
-
           <div className="form-section">
             <div className="input-group">
               <label>Nome</label>
-              <input
-                type="text"
-                placeholder="Nome"
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-              />
+              <input type="text" value={nome} readOnly />
             </div>
 
             <div className="input-group">
-              <label>E-mail*</label>
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+              <label>Email</label>
+              <input type="email" value={email} readOnly />
             </div>
 
             <div className="input-group">
-              <label>Senha*</label>
-              <input
-                type="password"
-                placeholder="Nova senha"
-                value={senha}
-                onChange={(e) => setSenha(e.target.value)}
-              />
+              <label>Nova senha</label>
+              <input type="password" value={novaSenha} onChange={(e) => setNovaSenha(e.target.value)}placeholder="Digite a nova senha"/>
             </div>
-
             <button className="atualizar-senha" onClick={atualizarSenha}>
               Atualizar Senha
             </button>
